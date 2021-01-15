ccb4d254b4f742464f87342c5bf0523caf173938
blue.1 <blue.1@kakaocorp.com>
2021-01-15 12:38:58 +0900

    showToast -> showAlert 수정

--- KakaoProfile/KakaoProfile/extension/UIViewController+.swift
+++ KakaoProfile/KakaoProfile/extension/UIViewController+.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 extension UIViewController {
-    func showToast(controller: UIViewController, message: String, seconds: Double) {
+    func showAlert(controller: UIViewController, message: String, seconds: Double) {
         let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
         alert.view.backgroundColor = UIColor.black
         alert.view.alpha = 0.6
